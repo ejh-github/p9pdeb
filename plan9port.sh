dcc0160785c478a2270aa6467d079fd50070bcb6
Elias Howell <ejh@snarf.top>
2021-03-01 01:44:50 -0600

    Update plan9port.sh

--- plan9port.sh
+++ plan9port.sh
@@ -3,7 +3,7 @@ git clone https://github.com/9fans/plan9port plan9
 cd plan9
 sudo apt install xorg-dev -y
 ./INSTALL
-echo ""
+echo "" >> ~/.bashrc
 echo "# plan9port" >> ~/.bashrc
 echo "PLAN9=/home/$USER/plan9 export PLAN9" >> ~/.bashrc
 echo "PATH=$PATH:$PLAN9/bin export PATH" >> ~/.bashrc
